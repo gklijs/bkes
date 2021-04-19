6ef8acfce94ba6964655d305057ac15fc49f87f2
Gerard Klijs <g.klijs@gmail.com>
2021-04-20 01:21:03 +0200

    prepare 0.0.5 release

--- java-client/src/main/java/tech/gklijs/bkes/client/BlockingClient.java
+++ java-client/src/main/java/tech/gklijs/bkes/client/BlockingClient.java
@@ -9,24 +9,47 @@ import tech.gklijs.bkes.api.RetrieveRequest;
 import tech.gklijs.bkes.api.StartReply;
 import tech.gklijs.bkes.api.StartRequest;
 
+/**
+ * Implementation for a simple blocking client
+ */
 public class BlockingClient {
 
     private final BkesGrpc.BkesBlockingStub stub;
 
+    /**
+     * Creates a net blocking client
+     * @param host host of the bkes server
+     * @param port port of the bkes server
+     */
     public BlockingClient(String host, int port) {
         stub = BkesGrpc.newBlockingStub(
                 NettyChannelBuilder.forAddress(host, port).build()
         );
     }
 
+    /**
+     * Start a new aggregate
+     * @param request data to start
+     * @return error or succes
+     */
     public StartReply start(StartRequest request){
         return stub.start(request);
     }
 
+    /**
+     * Add to an aggregate
+     * @param request data to add
+     * @return error or succes
+     */
     public AddReply add(AddRequest request){
         return stub.add(request);
     }
 
+    /**
+     * Retrieve all the records for the key
+     * @param request data to retrieve
+     * @return error or success
+     */
     public RetrieveReply retrieve(RetrieveRequest request){
         return stub.retrieve(request);
     }
--- java-client/src/main/java/tech/gklijs/bkes/client/FutureClient.java
+++ java-client/src/main/java/tech/gklijs/bkes/client/FutureClient.java
@@ -10,24 +10,47 @@ import tech.gklijs.bkes.api.RetrieveRequest;
 import tech.gklijs.bkes.api.StartReply;
 import tech.gklijs.bkes.api.StartRequest;
 
+/**
+ * Client working with futures for async processing
+ */
 public class FutureClient {
 
     private final BkesGrpc.BkesFutureStub stub;
 
+    /**
+     * Creates a new future client
+     * @param host host of bkes server
+     * @param port port of bkes server
+     */
     public FutureClient(String host, int port) {
         stub = BkesGrpc.newFutureStub(
                 NettyChannelBuilder.forAddress(host, port).build()
         );
     }
 
+    /**
+     * Start an aggregate
+     * @param request data to start
+     * @return success or error
+     */
     public ListenableFuture<StartReply> start(StartRequest request) {
         return stub.start(request);
     }
 
+    /**
+     * Add to aggregate
+     * @param request data to add
+     * @return success or error
+     */
     public ListenableFuture<AddReply> add(AddRequest request) {
         return stub.add(request);
     }
 
+    /**
+     * Retrieve al records with a key
+     * @param request data to retrieve
+     * @return success or error
+     */
     public ListenableFuture<RetrieveReply> retrieve(RetrieveRequest request) {
         return stub.retrieve(request);
     }
