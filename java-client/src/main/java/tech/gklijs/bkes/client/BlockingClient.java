badf76d0d6c648de753f3173ad712ed78c2689df
Gerard Klijs <g.klijs@gmail.com>
2021-04-22 20:19:02 +0200

    Prepare v0.0.6 of the java client.

--- java-client/src/main/java/tech/gklijs/bkes/client/BlockingClient.java
+++ java-client/src/main/java/tech/gklijs/bkes/client/BlockingClient.java
@@ -1,5 +1,6 @@
 package tech.gklijs.bkes.client;
 
+import com.google.protobuf.ByteString;
 import io.grpc.netty.NettyChannelBuilder;
 import tech.gklijs.bkes.api.AddReply;
 import tech.gklijs.bkes.api.AddRequest;
@@ -17,7 +18,8 @@ public class BlockingClient {
     private final BkesGrpc.BkesBlockingStub stub;
 
     /**
-     * Creates a net blocking client
+     * Creates a new blocking client
+     *
      * @param host host of the bkes server
      * @param port port of the bkes server
      */
@@ -29,28 +31,46 @@ public class BlockingClient {
 
     /**
      * Start a new aggregate
-     * @param request data to start
+     *
+     * @param key   key of the item, should not yet exist
+     * @param value value of the item
      * @return error or succes
      */
-    public StartReply start(StartRequest request){
+    public StartReply start(String key, byte[] value) {
+        StartRequest request = StartRequest.newBuilder()
+                .setKey(ByteString.copyFromUtf8(key))
+                .setValue(ByteString.copyFrom(value))
+                .build();
         return stub.start(request);
     }
 
     /**
      * Add to an aggregate
-     * @param request data to add
+     *
+     * @param key   key of the item, should already exist
+     * @param value value of the item
+     * @param order 0-based order for the message. To prevent concurrent writes
      * @return error or succes
      */
-    public AddReply add(AddRequest request){
+    public AddReply add(String key, byte[] value, int order) {
+        AddRequest request = AddRequest.newBuilder()
+                .setKey(ByteString.copyFromUtf8(key))
+                .setValue(ByteString.copyFrom(value))
+                .setOrder(order)
+                .build();
         return stub.add(request);
     }
 
     /**
      * Retrieve all the records for the key
-     * @param request data to retrieve
+     *
+     * @param key key of the item, should not yet exist
      * @return error or success
      */
-    public RetrieveReply retrieve(RetrieveRequest request){
+    public RetrieveReply retrieve(String key) {
+        RetrieveRequest request = RetrieveRequest.newBuilder()
+                .setKey(ByteString.copyFromUtf8(key))
+                .build();
         return stub.retrieve(request);
     }
 }
--- java-client/src/main/java/tech/gklijs/bkes/client/FutureClient.java
+++ java-client/src/main/java/tech/gklijs/bkes/client/FutureClient.java
@@ -1,6 +1,7 @@
 package tech.gklijs.bkes.client;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.protobuf.ByteString;
 import io.grpc.netty.NettyChannelBuilder;
 import tech.gklijs.bkes.api.AddReply;
 import tech.gklijs.bkes.api.AddRequest;
@@ -29,29 +30,47 @@ public class FutureClient {
     }
 
     /**
-     * Start an aggregate
-     * @param request data to start
-     * @return success or error
+     * Start a new aggregate
+     *
+     * @param key   key of the item, should not yet exist
+     * @param value value of the item
+     * @return error or succes
      */
-    public ListenableFuture<StartReply> start(StartRequest request) {
+    public ListenableFuture<StartReply> start(String key, byte[] value) {
+        StartRequest request = StartRequest.newBuilder()
+                .setKey(ByteString.copyFromUtf8(key))
+                .setValue(ByteString.copyFrom(value))
+                .build();
         return stub.start(request);
     }
 
     /**
-     * Add to aggregate
-     * @param request data to add
-     * @return success or error
+     * Add to an aggregate
+     *
+     * @param key   key of the item, should already exist
+     * @param value value of the item
+     * @param order 0-based order for the message. To prevent concurrent writes
+     * @return error or succes
      */
-    public ListenableFuture<AddReply> add(AddRequest request) {
+    public ListenableFuture<AddReply> add(String key, byte[] value, int order) {
+        AddRequest request = AddRequest.newBuilder()
+                .setKey(ByteString.copyFromUtf8(key))
+                .setValue(ByteString.copyFrom(value))
+                .setOrder(order)
+                .build();
         return stub.add(request);
     }
 
     /**
-     * Retrieve al records with a key
-     * @param request data to retrieve
-     * @return success or error
+     * Retrieve all the records for the key
+     *
+     * @param key key of the item, should not yet exist
+     * @return error or success
      */
-    public ListenableFuture<RetrieveReply> retrieve(RetrieveRequest request) {
+    public ListenableFuture<RetrieveReply> retrieve(String key) {
+        RetrieveRequest request = RetrieveRequest.newBuilder()
+                .setKey(ByteString.copyFromUtf8(key))
+                .build();
         return stub.retrieve(request);
     }
 }
